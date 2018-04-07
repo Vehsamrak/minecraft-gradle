f147b8e0b032b5c0f7fd92541e81eccb011a39cb
Petr Karmashev <smonkl@bk.ru>
2018-04-07 21:14:45 +0300

    Minecraft test plugin and gradle build instructions

---
+++ src/io/github/vehsamrak/minecraft/Main.java
@@ -0,0 +1,9 @@
+package io.github.vehsamrak.minecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    public void onEnable() {
+        System.out.println( "))) PLUGIN ENABLED (((" );
+    }
+}
