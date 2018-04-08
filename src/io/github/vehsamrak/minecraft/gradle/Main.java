0e8e6383da60cc18fe857e66e5a228cf8d95a23d
Petr Karmashev <smonkl@bk.ru>
2018-04-08 12:45:24 +0300

    Play sound command

--- src/io/github/vehsamrak/minecraft/gradle/Main.java
+++ src/io/github/vehsamrak/minecraft/gradle/Main.java
@@ -1,10 +1,25 @@
 package io.github.vehsamrak.minecraft.gradle;
 
+import org.bukkit.Sound;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     public void onEnable() {
         getLogger().info("))) PLUGIN ENABLED (((");
         getLogger().warning("This plugin is experimental!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = (Player) sender;
+        World world = player.getWorld();
+
+        world.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 5, 1);
+
+        return true;
     }
 }
